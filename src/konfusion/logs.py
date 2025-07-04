13210961b2076c39f5fddead397289f0ed002256
Adam Cmiel <acmiel@redhat.com>
2025-07-04 15:02:54 +0200

    Adjust logging format
    1) Log the datetime in ISO format and include the timezone
    
    2) Align log messages by aligning the level name to a width of 8
       (the length of CRITICAL, which is the longest log level name).
    
    Signed-off-by: Adam Cmiel <acmiel@redhat.com>

--- src/konfusion/logs.py
+++ src/konfusion/logs.py
@@ -1,18 +1,32 @@
 from __future__ import annotations
 
+import datetime
 import logging
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from collections.abc import Iterable
 
-LOG_FORMAT = "%(asctime)s %(levelname)s %(message)s"
+LOG_FORMAT = "%(asctime)s [%(levelname)-8s] %(message)s"
+
+
+class _ISOTimeFormatter(logging.Formatter):
+    def formatTime(  # noqa: N802 # blame the stdlib logging module for camelCase
+        self,
+        record: logging.LogRecord,
+        datefmt: str | None = None,
+    ) -> str:
+        dt = datetime.datetime.fromtimestamp(record.created).astimezone()
+        if not datefmt:
+            return dt.isoformat(sep=" ", timespec="seconds")
+        else:
+            return dt.strftime(datefmt)
 
 
 def setup_logging(level: int, modules: Iterable[str]) -> None:
     """Set up logging for the specified modules."""
     handler = logging.StreamHandler()
-    handler.setFormatter(logging.Formatter(LOG_FORMAT))
+    handler.setFormatter(_ISOTimeFormatter(LOG_FORMAT))
 
     for module in modules:
         logger = logging.getLogger(module)
